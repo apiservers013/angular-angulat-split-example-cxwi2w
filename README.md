3416fdb1c2d7d5d9de6aba692ee3bd80b4494f0f
apiservers013 <apiservers013@gmail.com>
2022-05-30 22:30:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-angulat-split-example-cxwi2w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-angulat-split-example-cxwi2w)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent {
+
+  safeSrc: SafeResourceUrl;
+  tv: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) { 
+    this.safeSrc =  this.sanitizer.bypassSecurityTrustResourceUrl("https://www.cryptometer.io/volume-flow");
+    this.tv =  this.sanitizer.bypassSecurityTrustResourceUrl("https://ru.tradingview.com/chart/rhp4KhhD/?symbol=BYBIT%3ABTCUSDT");
+
+  }
+
+}
